e2a26da9cdc754bc1b579db74ae6766e4d9f1b3f
Nono Junang <nono.junang@insideaem.com>
2015-10-18 23:56:17 +1300

    Added profile for incremental generation of aem package

--- content/src/main/content/jcr_root/apps/insideaem/recorder/clientlib/js/constants.js
+++ content/src/main/content/jcr_root/apps/insideaem/recorder/clientlib/js/constants.js
@@ -1,3 +1,3 @@
-com.insideaem.recorder.PAGE_PATH = '/content/insideaem/recorder/jcr:content';
+com.insideaem.recorder.PAGE_PATH = '/etc/insideaem/recorder/jcr:content';
 com.insideaem.recorder.STORE_URL = com.insideaem.recorder.PAGE_PATH+'.store.json';
 com.insideaem.recorder.CONTROL_URL = com.insideaem.recorder.PAGE_PATH+'.control.json';
